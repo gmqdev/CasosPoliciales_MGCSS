4f18d7f66cb54c2c2120cf32085cc8773bb1be2e
gmqdev <gmqdev@gmail.com>
2018-05-02 21:07:39 +0200

    Ejercicio 3:
    Del mismo modo, añadir los siguientes métodos a manejaExperto
    
    · Mostrar todos los nombres y especialidades de cada uno de los expertos
    de la tabla Experto
    	public void obtenNombresyEspecialidad() 
    
    · Mostrar todos los expertos cuya especialidad sea la pasada por
    parámetro
    	public void listaConParametro(String keyword)
    
    · Listar los nombres de todos los expertos y de todos los casos en los que
    hayan participado
    	public void obtenCasos()

--- src/main/java/com/mycompany/casospoliciales/manejaExperto.java
+++ src/main/java/com/mycompany/casospoliciales/manejaExperto.java
@@ -6,6 +6,7 @@
 package com.mycompany.casospoliciales;
 
 import java.sql.PreparedStatement;
+import java.util.List;
 import java.util.Scanner;
 import org.hibernate.HibernateException;
 import org.hibernate.Query;
@@ -102,5 +103,70 @@ public class manejaExperto {
         return experto;
     }
     
+    //Mostrar todos los nombres y especialidades de cada uno de los expertos de la tabla Experto
+    public void obtenNombresyEspecialidad(){
+        
+        try {
+            iniciaOperacion();
+            Query query = sesion.createQuery("from Experto");   // Hago la consulta HQL, me devuelve un objeto con una lista.
+            List<Experto> expertos = (List<Experto>) query.list(); // Una lista donde cargar la información.
+            System.out.println("\nNombre\t\tEspecialidad");
+            System.out.println("----------------------------------");
+            for(Experto experto:expertos){
+                System.out.println(experto.getNombre() + "\t\t" + experto.getEspecialidad());
+            }
+            System.out.println("\n\nFin obtenNombresyEspecialidad");
+        } catch (HibernateException he) {
+            manejaExcepcion(he);
+            throw he;
+        }
+        finally{
+            finalizaOperacion();
+        }
+    }
     
+    public void listaConParametro(String keyword){
+        
+        try {
+            iniciaOperacion();
+            Query query = sesion.createQuery("from Experto where especialidad='" + keyword+ "'");
+            List<Experto> expertos = (List<Experto>) query.list();
+            System.out.println("\nNombre\t\t\tEspecialidad");
+            System.out.println("-------------------------------------------");
+            for(Experto experto:expertos){
+                System.out.println(experto.getNombre() + "\t\t" + experto.getEspecialidad());
+            }
+            System.out.println("\n\nFin listaConParametro");
+        } catch (HibernateException he) {
+            manejaExcepcion(he);
+            throw he;
+        }
+        finally{
+            finalizaOperacion();
+        }
+    }
+    
+    public void obtenCasos(){
+        Experto experto;
+        
+        try {
+            iniciaOperacion();
+            Query query = sesion.createQuery("from Colabora");
+            List<Colabora> colaboradores = (List<Colabora>) query.list();
+            System.out.println("\nNombre\t\t\tCódigo Caso");
+            System.out.println("-------------------------------------------");
+            for(Colabora colaborador:colaboradores){
+                experto = (Experto) sesion.get(Experto.class, colaborador.getId().getCodexperto());
+                //System.out.println(obtenExperto(colaborador.getId().getCodexperto()).getNombre() + "\t\t" + colaborador.getId().getCodcaso());
+                System.out.println(experto.getNombre() + "\t\t" + colaborador.getId().getCodcaso());
+            }
+            System.out.println("\n\nFin obtenCasos");
+        } catch (HibernateException he) {
+            manejaExcepcion(he);
+            throw he;
+        }
+        finally{
+            finalizaOperacion();
+        }
+    }
 }
